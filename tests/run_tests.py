7f8b558fc9296e1284280d9e14861628d94ced97
Juraj Panoc <juraj.panoc@gmail.com>
2025-03-06 16:35:21 +0100

    refactored the run_tests script

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,50 +1,62 @@
+#!/usr/bin/env python3
+
 # "rec" is passed in as an argument for recording test output
+
 import subprocess as sbp
 import os
 import sys
 import time
 
-success_count = 0
 
-def testing(folder: str, out_hadzik: str):
-    global success_count
-    with open(f"tests/{folder}/{folder}.txt", "r") as f:
-        out_record = f.read()
+def record(folders: tuple[str, ...]):
+    for folder in folders:
+        output = recompile_and_run(folder)
 
-    if out_record == out_hadzik:
-        print(f"{"\033[92m"}[SUCCESS]{"\033[0m"} \nExpected: {out_record} \nGot: {out_hadzik}")
-        success_count += 1
-    else:
-        print(f"{"\033[31m"}[FAIL]{"\033[0m"} \nExpected: {out_record} \nGot: {out_hadzik}")
+        assert isinstance(output, str), ValueError(f"ERROR: compilation of {folder} failed")
+        with open(f"tests/{folder}/{folder}.txt", "w") as f:
+            f.write(output)
 
 
-def record(name: str, output: str):
-    with open(f"tests/{name}/{name}.txt", "w") as f:
-        f.write(output)
+def recompile_and_run(test_name: str) -> str | sbp.CompletedProcess[str]:
+    compilation = sbp.run(["./src/hdz.py", f"tests/{test_name}/{test_name}.hdz"], capture_output=True, text=True)
+    
+    while not os.path.exists(f"./tests/{test_name}/{test_name}"): # waits for the compilation to be done so it can run the file
+        time.sleep(1)
+    
+    if compilation.returncode != 0:
+        print(f"{"\033[31m"}[FAIL]{"\033[0m"}\n{compilation.stdout}")
+        return compilation
+    
+    out_hadzik = sbp.run([f"./tests/{test_name}/{test_name}"], capture_output=True, text=True)
+    out_hadzik_str: str = f"stdout: {out_hadzik.stdout}| stderr: {out_hadzik.stderr}| returncode: {out_hadzik.returncode}"
+    return out_hadzik_str
+
+
+def testing(folders: tuple[str, ...]):
+    success_count = 0
+
+    for folder in folders:
+        out_hadzik = recompile_and_run(folder)
+
+        with open(f"tests/{folder}/{folder}.txt", "r") as f:
+            out_record = f.read()
+
+        if out_record == out_hadzik:
+            print(f"{"\033[92m"}[SUCCESS] {folder}{"\033[0m"} \nExpected: {out_record} \nGot: {out_hadzik}")
+            success_count += 1
+        else:
+            print(f"{"\033[31m"}[FAIL] {folder}{"\033[0m"} \nExpected: {out_record} \nGot: {out_hadzik}")
+    
+    print(f"{success_count} / {len(folders)} Success rate: {success_count/len(folders):.2%}")
 
 
 def main():
-    folders = tuple(filter(lambda x: not x.endswith(".py"), os.listdir("./tests")))
+    folders: tuple[str, ...] = tuple(filter(lambda x: not x.endswith(".py"), os.listdir("./tests")))
 
-    for folder in folders:
-        compilation = sbp.run(["python3", "src/hdz.py", f"tests/{folder}/{folder}.hdz"])
-        
-        while not os.path.exists(f"./tests/{folder}/{folder}"): # waits for the compilation to be done so it can run the file
-            if compilation.returncode != 0:
-                print(f"{"\033[31m"}[FAIL]{"\033[0m"} compilation failed")
-                exit(1)
-            time.sleep(1)
-        
-        out_hadzik = sbp.run([f"./tests/{folder}/{folder}"], capture_output=True, text=True)
-        out_hadzik_str: str = f"stdout: {out_hadzik.stdout}| stderr: {out_hadzik.stderr}| returncode: {out_hadzik.returncode}"
-        
-        if "rec" in sys.argv:
-            record(folder, out_hadzik_str)
-        else:
-            testing(folder, out_hadzik_str)
-    
-    if "rec" not in sys.argv:
-        print(f"{success_count} / {len(folders)} Success rate: {success_count/len(folders):.2%}")
+    if "rec" in sys.argv:
+        record(folders)
+    else:
+        testing(folders)
 
 if __name__ == "__main__":
     main()
