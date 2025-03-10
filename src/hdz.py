0f108905534a8775a871580bf42ffc9f00a60ccb
juripan <juraj.panoc@gmail.com>
2025-03-10 15:02:24 +0100

    small refactor of the main hdz.py file

--- src/hdz.py
+++ src/hdz.py
@@ -24,14 +24,7 @@ Flags:
     exit(0)
 
 
-def hdz_run(executable: str):
-    print("Program output".center(80, "-"))
-    exit_code = os.system("./" + executable)
-    print("-" * 80)
-    print(f"Program exited with: {exit_code % 255}") # exit code 1 is 256 for some reason
-
-
-def hdz_to_asm(file_path: str) -> list[str]:
+def compile_to_asm(file_path: str) -> list[str]:
     try:
         with open(file_path, "r") as f:
             hdz_src: str = f.read()
@@ -48,6 +41,7 @@ def hdz_to_asm(file_path: str) -> list[str]:
         print(parse_tree)
         print("-" * 130)
     final_asm = Generator(parse_tree, hdz_src).gen_program()
+    
     return final_asm
 
 
@@ -55,30 +49,36 @@ def asm_to_bin(filepath_no_hdz: str, content: list[str]):
     with open("./" + filepath_no_hdz + ".asm", "w") as f:
         f.writelines(content)
     
-    if os.system("nasm -felf64 " + filepath_no_hdz + ".asm") or os.system("ld " + filepath_no_hdz + ".o -o " + filepath_no_hdz):
+    if os.system("nasm -felf64 " + filepath_no_hdz + ".asm") \
+    or os.system("ld " + filepath_no_hdz + ".o -o " + filepath_no_hdz):
         print("ERROR: assembly failed", file=sys.stderr)
         exit(1)
+
+
+def hdz_run(executable: str):
+    print("Program output".center(80, "-"))
+    exit_code = os.system("./" + executable)
+    print("-" * 80)
+    print(f"Program exited with: {exit_code % 255}") # exit code 1 is 256 for some reason
 
 
 def main():
     all_flags: tuple[str, ...] = tuple(filter(lambda x: x[0] == "-", sys.argv))
     non_flags: tuple[str, ...] = tuple(filter(lambda x: x[0] != "-", sys.argv))[1:]
 
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 2 or "--help" in all_flags:
         hdz_help()
-
-    if "--help" in all_flags:
-        hdz_help()
-    if "-s" in all_flags:
-        ErrorHandler.dialect_errors = True
-    if "-d" in all_flags:
-        ErrorHandler.debug_mode = True
 
     file_path: str = non_flags[0]
     if not file_path.endswith(".hdz"):
         print("ERROR: File extension is missing or invalid (file extension must be .hdz)", file=sys.stderr)
         exit(1)
 
+    if "-s" in all_flags:
+        ErrorHandler.dialect_errors = True
+    if "-d" in all_flags:
+        ErrorHandler.debug_mode = True
+    
     if "-n" in all_flags and len(non_flags) <= 1:
         print("ERROR: Missing file path when using the 'n' flag", file=sys.stderr)
         exit(1)
@@ -87,7 +87,7 @@ def main():
     else:
         filepath_no_hdz = file_path.rsplit(".", 1)[0] # removes the file extension
     
-    final_asm = hdz_to_asm(file_path)
+    final_asm = compile_to_asm(file_path)
     
     asm_to_bin(filepath_no_hdz, final_asm)
 
